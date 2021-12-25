60e62bc4557faa96f30ee6b990c6189de6be816f
Arslan <arslanastral@outlook.com>
2021-12-25 13:58:23 +0800

    setup useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
-function App() {
+import React, { useState } from "react";
+import styled from "styled-components";
+import ScatterPlot from "./components/ScatterPlot";
 
+const AppContainer = styled.div`
+  background: white
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  min-width: 100vw;
+`;
+
+function App() {
   return (
-    <div>
-     Hello World
-    </div>
-  )
+    <ScatterPlotProvider>
+      <ScatterPlotWrapper />
+    </ScatterPlotProvider>
+  );
 }
 
-export default App
+// eslint-disable-next-line react/display-name
+const ScatterPlotWrapper = React.memo(() => {
+  return (
+    <AppContainer>
+      <ScatterPlot />
+    </AppContainer>
+  );
+});
+
+function ScatterPlotProvider({ children }) {
+  const [data, setData] = useState([]);
+  const [currentData, setcurrentData] = useState({
+    type: "TransistorCount",
+    URL:
+      "https://gist.githubusercontent.com/arslanastral/6ac598aeeaf2c577dcbb785f203b263b/raw/12d2b06d274040582999ea40f56b4caa910aa03a/mircroprocessor-transistor-count.csv",
+  });
+
+  return (
+    <ScatterPlotContext.Provider
+      value={{
+        data,
+        setData,
+        currentData,
+        setcurrentData,
+      }}
+    >
+      {children}
+    </ScatterPlotContext.Provider>
+  );
+}
+
+export const ScatterPlotContext = React.createContext();
+export default App;
