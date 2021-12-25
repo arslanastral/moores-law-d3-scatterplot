998e118b3f2133765384d66255fbc2c3beff1ccc
Arslan <81504779+arslanastral@users.noreply.github.com>
2021-12-25 14:01:12 +0800

    adding Plot container styling

--- src/components/ScatterPlot.jsx
+++ src/components/ScatterPlot.jsx
@@ -1,4 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+import styled from "styled-components";
+import { ScatterPlotContext } from "../App";
+
+const ScatterPlotContainer = styled.div`
+  border-radius: 10px;
+  width: clamp(200px, 60vw, 800px);
+  height: 400px;
+  margin-left: -35px;
+`;
+
+const Wrapper = styled.div`
+  background: white;
+  width: clamp(320px, 85vw, 1000px);
+  box-shadow: 0 2px 25px rgba(255, 0, 130, 0.5);
+  height: 750px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const ScatterPlot = () => {
   return <div></div>;
