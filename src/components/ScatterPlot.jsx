5161d10af89e82e0d754daf574ad3d02e1f47ebe
Arslan <81504779+arslanastral@users.noreply.github.com>
2021-12-25 14:12:38 +0800

    added useResizeObserver hook for responsiveness

--- src/components/ScatterPlot.jsx
+++ src/components/ScatterPlot.jsx
@@ -61,4 +61,21 @@ const ScatterPlot = () => {
   );
 };
 
+const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null);
+  useEffect(() => {
+    const observeTarget = ref.current;
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry.contentRect);
+      });
+    });
+    resizeObserver.observe(observeTarget);
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+  return dimensions;
+};
+
 export default ScatterPlot;
